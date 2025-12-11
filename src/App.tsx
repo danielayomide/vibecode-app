18eac4134cab6e418ec4059813512ed42f340633
dahnizzy <danielayomidesolomon@gmail.com>
2025-12-11 18:39:50 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,131 @@
+const img = "https://www.figma.com/api/mcp/asset/0cbb49c9-553f-42b3-84a7-6ebf3885da12";
+const img1 = "https://www.figma.com/api/mcp/asset/5e103091-d7b2-46b8-b227-240c9bf659a9";
+const imgFrame1618869436 = "https://www.figma.com/api/mcp/asset/1afe186a-2a3e-48de-bc9a-e341331548e8";
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-[var(--background\/canvas,white)] relative w-[390px] h-[844px] shadow-xl rounded-[40px] overflow-hidden" data-name="iPhone 13 & 14 - 138" data-node-id="25688:1584">
+        <div className="absolute bg-[var(--color\/grey\/700,#090909)] bottom-[34px] content-stretch flex gap-[8px] h-[48px] items-center justify-center left-1/2 overflow-clip px-[32px] py-[8px] rounded-[8px] translate-x-[-50%] w-[350px]" data-name="onboarding-CTA" data-node-id="25688:1589">
+          <div className="flex flex-col font-['Inter',sans-serif] font-semibold justify-center leading-[0] not-italic relative shrink-0 text-[14px] text-[color:var(--text\/on-primary,white)] text-center whitespace-nowrap" data-node-id="25688:1592">
+            <p className="leading-[18px]">Send money</p>
+          </div>
+        </div>
+        <div className="absolute content-stretch flex flex-col gap-[8px] items-start left-[20px] top-[113px] w-[350px]" data-node-id="25688:1595">
+          <p className="font-['Inter',sans-serif] font-medium leading-[22px] not-italic relative shrink-0 text-[#30313d] text-[14px] w-full whitespace-pre-wrap" data-node-id="25688:1596">
+            Today
+          </p>
+          <div className="content-stretch flex flex-col items-start overflow-clip relative shrink-0 w-full" data-node-id="25688:1597">
+            <div className="content-stretch flex gap-[16px] items-center px-0 py-[8px] relative shrink-0 w-full" data-node-id="25688:1598">
+              <div className="content-stretch flex flex-[1_0_0] gap-[8px] items-center min-h-px min-w-px relative shrink-0" data-node-id="25688:1599">
+                <div className="bg-[#468dfa] overflow-clip relative rounded-[125px] shrink-0 size-[36px]" data-name="Avatar" data-node-id="25688:1600">
+                  <div className="absolute left-0 size-[36px] top-0" data-name="image 1" data-node-id="I25688:1600;7734:42395">
+                    <img alt="" className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none size-full" src={img} />
+                  </div>
+                </div>
+                <div className="content-stretch flex flex-col items-start relative shrink-0 w-[238px]" data-node-id="25688:1601">
+                  <p className="font-['Inter',sans-serif] font-medium leading-[22px] not-italic relative shrink-0 text-[#30313d] text-[14px]" data-node-id="25688:1602">
+                    Gavin Nelson
+                  </p>
+                  <div className="content-stretch flex gap-[4px] items-center relative shrink-0" data-node-id="25688:1603">
+                    <div className="content-stretch flex items-center relative shrink-0" data-node-id="25688:1604">
+                      <p className="font-['Inter',sans-serif] font-normal leading-[19px] not-italic relative shrink-0 text-[#6a7383] text-[13px]" data-node-id="25688:1605">
+                        Checking
+                      </p>
+                    </div>
+                    <div className="relative shrink-0 size-[3px]" data-node-id="25688:1606">
+                      <img alt="" className="block max-w-none size-full" src={imgFrame1618869436} />
+                    </div>
+                    <p className="font-['Inter',sans-serif] font-normal leading-[19px] not-italic relative shrink-0 text-[#6a7383] text-[13px]" data-node-id="25688:1608">
+                      3150
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <p className="font-['Inter',sans-serif] font-medium leading-[22px] not-italic relative shrink-0 text-[#30313d] text-[15px]" data-node-id="25688:1609">
+                £ 438
+              </p>
+            </div>
+            <div className="content-stretch flex items-center px-0 py-[8px] relative shrink-0 w-full" data-node-id="25688:1610">
+              <div className="content-stretch flex flex-[1_0_0] items-center justify-between min-h-px min-w-px relative shrink-0" data-node-id="25688:1611">
+                <div className="content-stretch flex gap-[8px] items-center relative shrink-0 w-[247px]" data-node-id="25688:1612">
+                  <div className="bg-[#468dfa] overflow-clip relative rounded-[125px] shrink-0 size-[36px]" data-name="Avatar" data-node-id="25688:1613">
+                    <div className="absolute left-0 size-[36px] top-0" data-name="image 1" data-node-id="I25688:1613;7734:42395">
+                      <img alt="" className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none size-full" src={img1} />
+                    </div>
+                  </div>
+                  <div className="content-stretch flex flex-[1_0_0] flex-col items-start min-h-px min-w-px relative shrink-0" data-node-id="25688:1614">
+                    <div className="content-stretch flex items-center relative shrink-0" data-node-id="25688:1615">
+                      <p className="font-['Inter',sans-serif] font-medium leading-[22px] not-italic relative shrink-0 text-[#30313d] text-[14px]" data-node-id="25688:1616">
+                        Ayomide Daniel
+                      </p>
+                    </div>
+                    <div className="content-stretch flex gap-[4px] items-center relative shrink-0" data-node-id="25688:1617">
+                      <div className="content-stretch flex items-center relative shrink-0" data-node-id="25688:1618">
+                        <p className="font-['Inter',sans-serif] font-normal leading-[19px] not-italic relative shrink-0 text-[#6a7383] text-[13px]" data-node-id="25688:1619">
+                          Checking
+                        </p>
+                      </div>
+                      <div className="relative shrink-0 size-[3px]" data-node-id="25688:1620">
+                        <img alt="" className="block max-w-none size-full" src={imgFrame1618869436} />
+                      </div>
+                      <p className="font-['Inter',sans-serif] font-normal leading-[19px] not-italic relative shrink-0 text-[#6a7383] text-[13px]" data-node-id="25688:1622">
+                        3150
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <p className="font-['Inter',sans-serif] font-medium leading-[22px] not-italic relative shrink-0 text-[#30313d] text-[15px]" data-node-id="25688:1623">
+                  £ 438
+                </p>
+              </div>
+            </div>
+            <div className="content-stretch flex items-center px-0 py-[8px] relative shrink-0 w-full" data-node-id="25688:1624">
+              <div className="content-stretch flex flex-[1_0_0] items-center justify-between min-h-px min-w-px relative shrink-0" data-node-id="25688:1625">
+                <div className="content-stretch flex gap-[8px] items-center relative shrink-0 w-[247px]" data-node-id="25688:1626">
+                  <div className="bg-[#468dfa] content-stretch flex flex-col items-center justify-center overflow-clip relative rounded-[125px] shrink-0 size-[36px]" data-name="Avatar" data-node-id="25688:1627">
+                    <p className="font-['Inter',sans-serif] font-semibold leading-[23.625px] not-italic relative shrink-0 text-[13.5px] text-[color:var(--text\/on-solid-bg,white)]" data-node-id="I25688:1627;7734:42393">
+                      AD
+                    </p>
+                  </div>
+                  <div className="content-stretch flex flex-[1_0_0] flex-col items-start min-h-px min-w-px relative shrink-0" data-node-id="25688:1628">
+                    <div className="content-stretch flex items-center relative shrink-0" data-node-id="25688:1629">
+                      <p className="font-['Inter',sans-serif] font-medium leading-[22px] not-italic relative shrink-0 text-[#30313d] text-[14px]" data-node-id="25688:1630">
+                        Kaari Saarinen
+                      </p>
+                    </div>
+                    <div className="content-stretch flex gap-[4px] items-center relative shrink-0" data-node-id="25688:1631">
+                      <div className="content-stretch flex items-center relative shrink-0" data-node-id="25688:1632">
+                        <p className="font-['Inter',sans-serif] font-normal leading-[19px] not-italic relative shrink-0 text-[#6a7383] text-[13px]" data-node-id="25688:1633">
+                          Checking
+                        </p>
+                      </div>
+                      <div className="relative shrink-0 size-[3px]" data-node-id="25688:1634">
+                        <img alt="" className="block max-w-none size-full" src={imgFrame1618869436} />
+                      </div>
+                      <p className="font-['Inter',sans-serif] font-normal leading-[19px] not-italic relative shrink-0 text-[#6a7383] text-[13px]" data-node-id="25688:1636">
+                        3150
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <p className="font-['Inter',sans-serif] font-medium leading-[22px] not-italic relative shrink-0 text-[#30313d] text-[15px]" data-node-id="25688:1637">
+                  £ 438
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="absolute content-stretch flex flex-col items-start left-[20px] not-italic top-[37px] w-[70px] whitespace-pre-wrap" data-node-id="25688:1638">
+          <p className="font-['Inter',sans-serif] font-semibold leading-[22px] relative shrink-0 text-[#6a7383] text-[14px] w-full" data-node-id="25688:1639">
+            Balance
+          </p>
+          <p className="font-['Inter',sans-serif] font-bold leading-[32px] relative shrink-0 text-[#30313d] text-[24px] tracking-[-0.2px] w-full" data-node-id="25688:1640">
+            £ 438
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
